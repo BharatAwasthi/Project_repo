b40c1fbd37271220ca5ac7af9f4f35327536a888
BharatAwasthi <117386241+BharatAwasthi@users.noreply.github.com>
2024-07-17 09:54:42 +0530

    new_urls.py
    Changes in here

--- core/core/urls.py
+++ core/core/urls.py
@@ -13,7 +13,7 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,5 +21,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('home.urls')),  # Use the URLs from the home app
     path('', include('home.urls')),  # Assuming you use home app for the home page as well
+    
+]
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('home.urls')),  # Use the URLs from the home app
+    path('home/', include('home.urls')),  # Assuming you use home app for the home page as well
     
 ]
